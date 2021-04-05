edf09ffee212684b19f64318fbf88e8f5d6987ba
IbrahimKhalil <msg2ibrahim@gmail.com>
2021-04-05 20:44:23 +0200

    Successful Sample Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,11 @@ import babel from '@rollup/plugin-babel';
 import external from 'rollup-plugin-peer-deps-external';
 import del from 'rollup-plugin-delete';
 import pkg from './package.json';
+import list from './modules.json';
 
 const defineEntryPoint = (component) => {
-    const src = "src/"+component+".js";
-    const output = "lib/"+component+".js";
+    const src = `src/${component}.js`;
+    const output = `lib/${component}.js`;
     return {
         input: src,
         output: {file: output, format: 'esm', name: component},
@@ -20,7 +21,4 @@ const defineEntryPoint = (component) => {
     }
 };
 
-export default [
-    defineEntryPoint("components/HelloWorld"),
-    defineEntryPoint("components/HelloTestSection")
-];
+export default list.map(entry => defineEntryPoint(entry));
