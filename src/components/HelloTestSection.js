25bd7adb80c968f1530326564f99667e0a80b1de
IbrahimKhalil <msg2ibrahim@gmail.com>
2021-04-05 02:06:37 +0200

    First Test

---
+++ src/components/HelloTestSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HelloWorld() {
+    return (
+        <section>
+            <h2>Hello Test Section</h2>
+            <p>
+                My Test B
+            </p>
+        </section>
+    );
+}
+
+export default HelloWorld;
