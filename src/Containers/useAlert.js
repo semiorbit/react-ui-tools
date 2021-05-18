4313396d7f12a2f7d878b43ed3c28f7420a26ce3
IbrahimKhalil <msg2ibrahim@gmail.com>
2021-05-18 11:52:50 +0200

    1.3.10

---
+++ src/Containers/useAlert.js
@@ -0,0 +1,28 @@
+import React, {useMemo, useState} from 'react';
+import Alert from "@semiorbit/react-ui-tools/Containers/Alert";
+
+const useAlert = () => {
+
+    const [msg, setMsg] = useState(['', 'error']);
+
+
+
+    return useMemo(() => {
+        return {
+            show: () => (
+                <Alert
+                    severity={msg[1]}
+                    open={msg[0].length > 0}
+                    onClose={() => setMsg(['', 'error'])}
+                >{msg[0]}</Alert>
+            ),
+            error: (message) => setMsg([message, 'error']),
+            warning: (message) => setMsg([message, 'warning']),
+            info: (message) => setMsg([message, 'info']),
+            success: (message) => setMsg([message, 'success'])
+        }
+    }, [msg]);
+
+};
+
+export default useAlert;
