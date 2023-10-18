1dc23c6e4d02f7efd4a2e775d47cc4c00180abbc
Ibrahim Khalil <msg2ibrahim@hotmail.com>
2023-10-18 08:24:48 +0300

    2.0.7

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import babel from '@rollup/plugin-babel';
 import external from 'rollup-plugin-peer-deps-external';
 import del from 'rollup-plugin-delete';
-import pkg from './package.json';
-import list from './modules.json';
+import pkg from './package.json' assert {type: 'json'};
+import list from './modules.json' assert {type: 'json'};
 
 const defineEntryPoint = (component) => {
     const src = `src/${component}.js`;
